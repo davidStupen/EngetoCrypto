756b1946be301f2b8ea7e118e785266c587b919a
davidStupen <davidstupen4@seznam.cz>
2025-09-25 12:45:52 +0200

    dokonceni metody currentPrice v model

--- CryptoBackend/src/main/java/cz/davidos/Crypto/model/Crypto.java
+++ CryptoBackend/src/main/java/cz/davidos/Crypto/model/Crypto.java
@@ -1,5 +1,8 @@
 package cz.davidos.Crypto.model;
 
+import cz.davidos.Crypto.model.bitcoin.BitcoinDTO;
+import cz.davidos.Crypto.model.ethereum.Ethereum;
+import cz.davidos.Crypto.model.ethereum.EthereumDTO;
 import lombok.Data;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,11 +21,18 @@ public class Crypto {
         this.id = UUID.randomUUID();
         this.name = name;
         this.symbol = symbol;
-        this.price = ;
+        this.price = this.currentPrice(symbol);
         this.quantity = quantity;
     }
     private BigDecimal currentPrice(String symbol){
+        final String URL_ETH = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd";
+        final String URL_BTC = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
         RestTemplate template = new RestTemplate();
-
+        double value = switch (symbol.toUpperCase()){
+            case "BTC" -> template.getForObject(URL_BTC, BitcoinDTO.class).bitcoin().usd();
+            case "ETH" -> template.getForObject(URL_ETH, EthereumDTO.class).ethereum().usd();
+            default -> -1;
+        };
+        return BigDecimal.valueOf(value);
     }
 }
