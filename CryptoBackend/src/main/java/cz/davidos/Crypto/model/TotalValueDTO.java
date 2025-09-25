f6d945710e5e334dc7b505771d28b5306528885f
davidStupen <davidstupen4@seznam.cz>
2025-09-25 22:05:58 +0200

    pridani dalsi class pro vraceni odpovedi v controlleru

---
+++ CryptoBackend/src/main/java/cz/davidos/Crypto/model/TotalValueDTO.java
@@ -0,0 +1,11 @@
+package cz.davidos.Crypto.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+@Data
+public class TotalValueDTO {
+    private String err;
+    private BigDecimal totalValue;
+    
+}
