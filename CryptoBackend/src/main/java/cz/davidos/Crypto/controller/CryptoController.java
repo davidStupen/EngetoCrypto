16b159fa9488ab2ea61f2118b08d58b0b793f0c9
davidStupen <davidstupen4@seznam.cz>
2025-09-25 13:01:32 +0200

    pridani metody getAllCryptos v -> controller

--- CryptoBackend/src/main/java/cz/davidos/Crypto/controller/CryptoController.java
+++ CryptoBackend/src/main/java/cz/davidos/Crypto/controller/CryptoController.java
@@ -4,6 +4,8 @@ import cz.davidos.Crypto.model.Crypto;
 import cz.davidos.Crypto.service.CryptoService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/cryptos")
@@ -18,5 +20,8 @@ public class CryptoController {
     public void addCrypto(@RequestBody Crypto crypto){
         this.service.addCrypto(crypto);
     }
-
+    @GetMapping
+    public List<Crypto> getAllCryptos(){
+        return this.service.getAllCryptos();
+    }
 }
