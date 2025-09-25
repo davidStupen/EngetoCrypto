bb1dd1209beb8e21fda8c4d1aee7ea7d86af5a0e
davidStupen <davidstupen4@seznam.cz>
2025-09-25 16:16:03 +0200

    pridani kryptomen do api

--- CryptoBackend/src/main/java/cz/davidos/Crypto/model/Crypto.java
+++ CryptoBackend/src/main/java/cz/davidos/Crypto/model/Crypto.java
@@ -1,7 +1,9 @@
 package cz.davidos.Crypto.model;
 
 import cz.davidos.Crypto.model.bitcoin.BitcoinDTO;
+import cz.davidos.Crypto.model.dogecoin.DogeCoinDTO;
 import cz.davidos.Crypto.model.ethereum.EthereumDTO;
+import cz.davidos.Crypto.model.solana.SolanaDTO;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
@@ -31,10 +33,14 @@ public class Crypto {
     private BigDecimal currentPrice(String symbol){
         final String URL_ETH = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd";
         final String URL_BTC = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
+        final String URL_SOL = "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd";
+        final String URL_DOGE = "https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=usd";
         RestTemplate template = new RestTemplate();
         double value = switch (symbol.toUpperCase()){
             case "BTC" -> template.getForObject(URL_BTC, BitcoinDTO.class).bitcoin().usd();
             case "ETH" -> template.getForObject(URL_ETH, EthereumDTO.class).ethereum().usd();
+            case "SOL" -> template.getForObject(URL_SOL, SolanaDTO.class).solana().usd();
+            case "DOGE" -> template.getForObject(URL_DOGE, DogeCoinDTO.class).dogecoin().usd();
             default -> -1;
         };
         return BigDecimal.valueOf(value);
