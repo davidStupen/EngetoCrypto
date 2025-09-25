26edf8e9574daa33221eb7adb143b7963b89c347
davidStupen <davidstupen4@seznam.cz>
2025-09-25 12:56:29 +0200

    pridani POST do controlleru

--- CryptoBackend/src/main/java/cz/davidos/Crypto/controller/CryptoController.java
+++ CryptoBackend/src/main/java/cz/davidos/Crypto/controller/CryptoController.java
@@ -1,11 +1,22 @@
 package cz.davidos.Crypto.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import cz.davidos.Crypto.model.Crypto;
+import cz.davidos.Crypto.service.CryptoService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/cryptos")
 public class CryptoController {
+    private CryptoService service;
 
+    public CryptoController(CryptoService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public void addCrypto(@RequestBody Crypto crypto){
+        this.service.addCrypto(crypto);
+    }
 
 }
