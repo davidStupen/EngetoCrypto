68e233b6836f7d5e1f3e88d32afc581b2522f7f4
davidStupen <davidstupen4@seznam.cz>
2025-09-25 12:50:34 +0200

    pridani metody pro ulozeni crypta do listu -> service

--- CryptoBackend/src/main/java/cz/davidos/Crypto/service/CryptoService.java
+++ CryptoBackend/src/main/java/cz/davidos/Crypto/service/CryptoService.java
@@ -1,9 +1,17 @@
 package cz.davidos.Crypto.service;
 
+import cz.davidos.Crypto.model.Crypto;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CryptoService {
+    List<Crypto> cryptoList = new ArrayList<>();
 
+    public void addCrypto(Crypto crypto){
+        this.cryptoList.add(new Crypto(crypto.getName(), crypto.getSymbol(), crypto.getQuantity()));
+    }
 
 }
