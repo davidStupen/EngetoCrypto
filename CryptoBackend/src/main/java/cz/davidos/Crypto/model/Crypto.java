3fb714175337a29d18f41c0fa9ebe3dfa834b59a
davidStupen <davidstupen4@seznam.cz>
2025-09-27 12:34:37 +0200

    zmena API, kvuli vyssimu reta-limits ve classe Crypto

--- CryptoBackend/src/main/java/cz/davidos/Crypto/model/Crypto.java
+++ CryptoBackend/src/main/java/cz/davidos/Crypto/model/Crypto.java
@@ -10,6 +10,7 @@ import lombok.Data;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 import java.util.UUID;
 
 @Data
@@ -32,10 +33,12 @@ public class Crypto {
     }
     private BigDecimal currentPrice(String symbol){
         if (symbol == null) return BigDecimal.valueOf(-1);
-        final String URL_ETH = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd";
-        final String URL_BTC = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
-        final String URL_SOL = "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd";
-        final String URL_DOGE = "https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=usd";
+        final String KEY = "CG-XYiAwrdRgKxfMybPHZuXceGG";
+
+        final String URL_ETH = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd&x_cg_demo_api_key=" + KEY;
+        final String URL_BTC = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&x_cg_demo_api_key=" + KEY;
+        final String URL_SOL = "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd&x_cg_demo_api_key=" + KEY;
+        final String URL_DOGE = "https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=usd&x_cg_demo_api_key=" + KEY;
         RestTemplate template = new RestTemplate();
         double value = switch (symbol.toUpperCase()){
             case "BTC" -> template.getForObject(URL_BTC, BitcoinDTO.class).bitcoin().usd();
